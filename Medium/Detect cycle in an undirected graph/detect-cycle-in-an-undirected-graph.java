c759357c89cff4a1801393915fd91f0920708bcb
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-21 23:03:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class pair
+{
+    int vertex;
+    int parent;
+    pair(int vertex,int parent)
+    {
+        this.vertex=vertex;
+        this.parent=parent;
+    }
+}
+class Solution {
+    // Function to detect cycle in an unirected graph.
+    
+    public boolean dfs(ArrayList<ArrayList<Integer>> adj,int node,int parent,
+    boolean[] visited)
+    {
+        visited[node]=true;
+        for(int x:adj.get(node))
+        {
+            if(!visited[x])
+            {
+                if(dfs(adj,x,node,visited))
+                return true;
+            }
+            else if(x!=parent) return true;
+        }
+        return false;
+    }
+    public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean visited[]=new boolean[v];
+        Arrays.fill(visited,false);
+        int i;
+        for(i=0;i<v;i++)
+        {
+            if(!visited[i])
+            {
+                if(dfs(adj,i,-1,visited))
+                return true;
+            }
+        }
+        return false;
+    }
+}
