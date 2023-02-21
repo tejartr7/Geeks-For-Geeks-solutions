678fc04d5790da8c3b7d41417357e07bfdef6723
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-02-21 19:06:14 +0530

    Updated Solution - GfG to GitHub

--- Medium/Fill the Matrix/fill-the-matrix.java
+++ Medium/Fill the Matrix/fill-the-matrix.java
@@ -7,66 +7,12 @@ import java.util.*;
 // } Driver Code Ends
 //User function Template for Java
 
-class pair{
-    int row;
-    int col;
-    pair(int row,int col)
-    {
-        this.row=row;
-        this.col=col;
-    }
-}
-class Solution
-{
-    public static boolean valid(int i,int j,int m,int n)
-    {
-        return i>=0 && i<m && j>=0 && j<n;
-    }
-	public static int minIteration(int m, int n, int x, int y){
-		//code here
-		int count=0;
-		int arr[][]=new int[m][n];
-		int i,j;
-		for(i=0;i<m;i++)
-		Arrays.fill(arr[i],0);
-		Queue<pair> q=new LinkedList<>();
-		x--;
-		y--;
-		q.offer(new pair(x,y));
-		while(!q.isEmpty())
-		{
-		    count++;
-		    int l=q.size();
-		    for(int k=0;k<l;k++){
-		        pair top=q.poll();
-		        i=top.row;
-		        j=top.col;
-		        arr[i][j]=1;
-		        if(valid(i+1,j,m,n) && arr[i+1][j]==0)
-		        {
-		            q.offer(new pair(i+1,j));
-		            arr[i+1][j]=1;
-		        }
-		        if(valid(i-1,j,m,n) && arr[i-1][j]==0)
-		        {
-		            q.offer(new pair(i-1,j));
-		            arr[i-1][j]=1;
-		        }
-		        if(valid(i,j+1,m,n) && arr[i][j+1]==0)
-		        {
-		            q.offer(new pair(i,j+1));
-		            arr[i][j+1]=1;
-		        }
-		        if(valid(i,j-1,m,n) && arr[i][j-1]==0)
-		        {
-		            q.offer(new pair(i,j-1));
-		            arr[i][j-1]=1;
-		        }
-		    }
-		}
-		return count-1;
-	}
-}
+class Solution {
+	public static int minIteration(int N, int M, int x, int y) {
+	    int top = x-1, down = N-x;
+	    int left = y-1, right = M-y;
+	    return Math.max(top+left, Math.max(top+right, Math.max(down+left, down+right)));
+	}}
 
 //{ Driver Code Starts.
 
