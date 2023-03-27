02b9fd64673f106581ee7e0210bacb675accf275
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-03-27 23:16:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.java
+++ Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.java
@@ -42,74 +42,72 @@ class GFG {
 
 
 // User function Template for Java
-class tuple { 
-    int first, second, third; 
-    tuple(int _first, int _second, int _third) {
-        this.first = _first; 
-        this.second = _second; 
-        this.third = _third; 
+
+
+
+// User function Template for Java
+
+class tuple
+{
+    int dis;
+    int row;
+    int col;
+    tuple(int x,int y,int z)
+    {
+        dis=x;
+        row=y;
+        col=z;
     }
 }
 class Solution {
-
+    boolean valid(int i,int j,int m,int n)
+    {
+        return i>=0 && i<m && j>=0 && j<n;
+    }
     int shortestPath(int[][] grid, int[] source, int[] destination) {
 
-        // Edge Case: if the source is only the destination.
-        if(source[0] == destination[0] && 
-           source[1] == destination[1]) return 0; 
+        // Your code here
+        if(source[0] == destination[0] &&
+                source[1] == destination[1]) return 0;
+        int mini=Integer.MAX_VALUE;
+        int i,j,m=grid.length,n=grid[0].length;
+        Queue<tuple> q=new LinkedList<>();
+        q.offer(new tuple(0,source[0],source[1]));
 
-        // Create a queue for storing cells with their distances from source
-        // in the form {dist,{cell coordinates pair}}.
-        Queue<tuple> q = new LinkedList<>();  
-        int n = grid.length; 
-        int m = grid[0].length;
-
-        // Create a distance matrix with initially all the cells marked as
-        // unvisited and the source cell as 0. 
-        int[][] dist = new int[n][m]; 
-        for(int i = 0;i<n;i++) {
-            for(int j =0;j<m;j++) {
-                dist[i][j] = (int)(1e9); 
+        int dist[][]=new int[m][n];
+        for(i=0;i<m;i++)
+            Arrays.fill(dist[i],Integer.MAX_VALUE);
+        dist[source[0]][source[1]]=0;
+        while(!q.isEmpty())
+        {
+            tuple top=q.poll();
+            int dis=top.dis;
+            int x=top.row,y=top.col;
+            if(x==destination[0] && y==destination[1])
+            {
+                return dis;
             }
-        }
-        dist[source[0]][source[1]] = 0; 
-        q.add(new tuple(0, source[0], source[1])); 
-
-        // The following delta rows and delts columns array are created such that
-        // each index represents each adjacent node that a cell may have 
-        // in a direction.
-        int dr[] = {-1, 0, 1, 0}; 
-        int dc[] = {0, 1, 0, -1}; 
- 
-        // Iterate through the maze by popping the elements out of the queue
-        // and pushing whenever a shorter distance to a cell is found.
-        while(!q.isEmpty()) {
-            tuple it = q.peek(); 
-            q.remove(); 
-            int dis = it.first; 
-            int r = it.second; 
-            int c = it.third; 
             
-            // Through this loop, we check the 4 direction adjacent nodes
-            // for a shorter path to destination.
-            for(int i = 0;i<4;i++) {
-                int newr = r + dr[i]; 
-                int newc = c + dc[i]; 
-                
-                // Checking the validity of the cell and updating if dist is shorter.
-                if(newr >= 0 && newr < n && newc >= 0 && newc < m 
-                && grid[newr][newc] == 1 && dis + 1 < dist[newr][newc]) {
-                    dist[newr][newc] = 1 + dis; 
-
-                    // Return the distance until the point when
-                    // we encounter the destination cell.
-                    if(newr == destination[0] && 
-                       newc == destination[1]) return dis + 1; 
-                    q.add(new tuple(1+dis, newr, newc)); 
-                }
+            if(valid(x+1,y,m,n) && grid[x+1][y]==1 && dis+1<dist[x+1][y])
+            {   q.offer(new tuple(dis+1,x+1,y));
+                dist[x+1][y]=dis+1;
+            }
+            
+            if(valid(x-1,y,m,n) && grid[x-1][y]==1 && dis+1<dist[x-1][y])
+            {   q.offer(new tuple(dis+1,x-1,y));
+                dist[x-1][y]=dis+1;
+            }
+            
+            if(valid(x,y+1,m,n) && grid[x][y+1]==1 && dis+1<dist[x][y+1])
+            { q.offer(new tuple(dis+1,x,y+1));
+                dist[x][y+1]=dis+1;
+            }
+            
+            if(valid(x,y-1,m,n) && grid[x][y-1]==1 && dis+1<dist[x][y-1])
+            {    q.offer(new tuple(dis+1,x,y-1));
+                dist[x][y-1]=dis+1;
             }
         }
-        // If no path is found from source to destination.
-        return -1; 
+        return -1;
     }
 }
