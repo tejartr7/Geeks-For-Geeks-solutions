5bf4f5da52d16a7059fb7caf8e421d4fa9b36e40
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-04-09 11:31:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Floyd Warshall/floyd-warshall.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrix[i][j]  =Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.shortest_distance(matrix);
+            for(int i = 0; i < n; i++){
+                for(int j  = 0; j < n; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        // Code here 
+        int i,j,k;
+        int n=matrix.length;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = (int)(1e9);
+                }
+                if (i == j) matrix[i][j] = 0;
+            }
+        }
+        for(k=0;k<n;k++)
+        {
+            for(i=0;i<n;i++)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(i==k || j==k)
+                    continue;
+                    matrix[i][j]=Math.min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(matrix[i][j]==(int)(1e9))
+                matrix[i][j]=-1;
+            }
+        }
+    }
+}
