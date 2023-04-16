77ee14749161cdc7c6a0b749afe40dfdb550e051
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-04-16 15:26:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Connecting the graph/connecting-the-graph.java
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] edge = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                edge[i][0] = sc.nextInt();
+                edge[i][1] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            int ans = obj.Solve(n, edge);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+    Edge(int _src, int _dest, int _wt) {
+        this.src = _src; this.dest = _dest; this.weight = _wt;
+    }
+    // Comparator function used for
+    // sorting edgesbased on their weight
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+};
+class Solution {
+
+    public int Solve(int n, int[][] edge) {
+        // Code here
+        DisjointSet ds=new DisjointSet(n);
+        int i,j,l=edge.length;
+        int comp=0;
+        int extra=0;
+        for(i=0;i<l;i++)
+        {
+            int u=edge[i][0];
+            int v=edge[i][1];
+            if(ds.findUPar(u)==ds.findUPar(v))
+            extra++;
+            else
+            ds.unionBySize(u,v);
+        }
+        for(i=0;i<n;i++)
+        {
+            if(ds.findUPar(i)==i)
+            comp++;
+        }
+        if(extra>=comp-1)
+        return comp-1;
+        return -1;
+    }
+}
