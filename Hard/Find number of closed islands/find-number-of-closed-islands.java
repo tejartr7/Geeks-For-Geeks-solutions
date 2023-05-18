811c207c9b841bfecbdb01581b050f0be51bd648
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-05-18 10:52:26 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Find number of closed islands/find-number-of-closed-islands.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] str = br.readLine().trim().split(" ");
+            int N = Integer.parseInt(str[0]);
+            int M = Integer.parseInt(str[1]);
+            int[][] matrix = new int[N][M];
+            for(int i=0; i<N; i++)
+            {
+                String[] s = br.readLine().trim().split(" ");
+                for(int j=0; j<M; j++)
+                    matrix[i][j] = Integer.parseInt(s[j]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.closedIslands(matrix, N, M));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public boolean dfs(int x, int y, int m, int n, int[][] grid, boolean[][] visit) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return false;
+        }
+        if (grid[x][y] == 0 || visit[x][y]) {
+            return true;
+        }
+
+        visit[x][y] = true;
+        boolean isClosed = true;
+        int[] dirx = {0, 1, 0, -1};
+        int[] diry = {-1, 0, 1, 0};
+
+        for (int i = 0; i < 4; i++) {
+            int r = x + dirx[i];
+            int c = y + diry[i];
+            if (!dfs(r, c, m, n, grid, visit)) {
+                isClosed = false;
+            }
+        }
+
+        return isClosed;
+    }
+    public int closedIslands(int[][] grid, int N, int M)
+    {
+        // Code here
+         int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visit = new boolean[m][n];
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 && !visit[i][j] && dfs(i, j, m, n, grid, visit)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
