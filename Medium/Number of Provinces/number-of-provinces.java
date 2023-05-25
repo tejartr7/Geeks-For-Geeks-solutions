55061de83efe4a8d6bbf0d247207cee7d378e5bc
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-05-25 18:44:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Provinces/number-of-provinces.java
+++ Medium/Number of Provinces/number-of-provinces.java
@@ -30,86 +30,45 @@ class GFG {
 
 
 //User function Template for Java
-class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
-            rank.add(0);
-            parent.add(i);
-            size.add(1);
-        }
-    }
 
-    public int findUPar(int node) {
-        if (node == parent.get(node)) {
-            return node;
-        }
-        int ulp = findUPar(parent.get(node));
-        parent.set(node, ulp);
-        return parent.get(node);
-    }
-
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
-        }
-    }
-
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-        } else {
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
-        }
-    }
-}
-class Edge implements Comparable<Edge> {
-    int src, dest, weight;
-    Edge(int _src, int _dest, int _wt) {
-        this.src = _src; this.dest = _dest; this.weight = _wt;
-    }
-    // Comparator function used for
-    // sorting edgesbased on their weight
-    public int compareTo(Edge compareEdge) {
-        return this.weight - compareEdge.weight;
-    }
-};
 class Solution {
+    static int x=0;
+    static void dfs(int i,boolean visited[],Map<Integer,List<Integer>> map)
+    {
+        if(visited[i]) return ;
+        visited[i]=true;
+        for(int x:map.get(i))
+        {
+            if(!visited[x])
+            dfs(x,visited,map);
+        }
+    }
     static int numProvinces(ArrayList<ArrayList<Integer>> adj, int v) {
         // code here
-        DisjointSet ds=new DisjointSet(v);
+        Map<Integer,List<Integer>> map=new HashMap<>();
         int i,j;
-        int count=0;
+        for(i=0;i<v;i++)
+        map.put(i,new ArrayList<>());
         for(i=0;i<v;i++)
         {
             for(j=0;j<v;j++)
             {
-                if(adj.get(i).get(j)==1)
+                if(i!=j && adj.get(i).get(j)==1)
                 {
-                    ds.unionBySize(i,j);
+                    map.get(i).add(j);
                 }
             }
         }
+       // System.out.println(map);
+        boolean visited[]=new boolean[v];
+        int count=0;
         for(i=0;i<v;i++)
         {
-            if(ds.findUPar(i)==i)
-            count++;
+            if(!visited[i])
+            {
+                count++;
+                dfs(i,visited,map);
+            }
         }
         return count;
     }
