b335b4dd76e4ee9e60b2b150b3b4fde55ad5a829
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-05-31 21:26:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Frequency Game/frequency-game.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+// Driver class with main function
+class GFG {
+    // Main function
+    public static void main(String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+
+        // Iterating over testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+            int arr[] = new int[n];
+
+            String s[]=in.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution obj = new Solution();
+            System.out.println(obj.LargButMinFreq(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// Helper class to find largest number with minimum frequency
+class Solution {
+    // Function to find largest number with minimum frequency
+    public static int LargButMinFreq(int arr[], int n) {
+        // Your code here
+        Map<Integer,Integer> map=new HashMap<>();
+        int i;
+        for(int x:arr)
+        {
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        int mini=Integer.MAX_VALUE;
+        int maxi=0;
+        for(int x:map.keySet())
+        {
+            if(map.get(x)<=mini)
+            {
+                maxi=Math.max(maxi,x);
+                mini=map.get(x);
+            }
+        }
+        return maxi;
+    }
+}
