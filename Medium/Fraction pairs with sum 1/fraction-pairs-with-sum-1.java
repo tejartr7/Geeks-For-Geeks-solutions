7e3adf24ca6d605cc12ee2650c60402577b574dc
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-08-08 21:04:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Fraction pairs with sum 1/fraction-pairs-with-sum-1.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] numerator = IntArray.input(br, n);
+            
+            
+            int[] denominator = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.countFractions(n, numerator, denominator);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int countFractions(int l, int[] n, int[] d) {
+        // code here
+        Map<Double,Integer> map=new HashMap<>();
+        int i,j;
+        int ans=0;
+        for(i=0;i<l;i++)
+        {
+            double dd=(double)((double)n[i]/(double)d[i]);
+            double temp=(double)((double)(d[i]-n[i])/(double)d[i]);
+            ans+=map.getOrDefault(temp,0);
+            map.put(dd,map.getOrDefault(dd,0)+1);
+        }
+        return ans;
+    }
+}
+        
