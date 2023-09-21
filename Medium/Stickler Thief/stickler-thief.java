7f0d6c6f801ab70c2d80c86470f1f5a25acf0d3b
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-09-21 18:47:50 +0530

    Updated Solution - GfG to GitHub

--- Medium/Stickler Thief/stickler-thief.java
+++ Medium/Stickler Thief/stickler-thief.java
@@ -38,12 +38,14 @@ class Solution
     {
         // Your code here
         int dp[]=new int[n];
-        if(n==1) return arr[0];
+        int i,j;
         dp[0]=arr[0];
-        dp[1]=Math.max(arr[1],arr[0]);
-        for(int i=2;i<n;i++)
+        if(n==1)
+        return dp[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(i=2;i<n;i++)
         {
-            dp[i]=Math.max(dp[i-1],arr[i]+dp[i-2]);
+            dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
         }
         return dp[n-1];
     }
