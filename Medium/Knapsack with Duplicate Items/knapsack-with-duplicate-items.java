b6773117d8bca7f87409f0b3bc0c938c3ff1c029
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-10-25 21:22:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+             int[] dp = new int[W + 1];
+   
+        for (int bagcapacity = 1; bagcapacity <= W; bagcapacity++) {
+            for (int i = 0; i < N; i++) {
+                if (wt[i] <= bagcapacity) {
+                    int rembagcapacity=bagcapacity-wt[i];
+                    int totbagcap=dp[rembagcapacity]+val[i];
+                    dp[bagcapacity] = Math.max(dp[bagcapacity], totbagcap);
+                }
+            }
+        }
+
+        return dp[W];
+         
+        
+    }
+}
+
